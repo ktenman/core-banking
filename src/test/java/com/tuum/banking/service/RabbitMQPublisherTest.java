cc63f41130687d19e13fae621656120bf5424967
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-03-18 16:41:55 +0200

    Add RabbitMQPublisherTest class for testing RabbitMQPublisher functionality

---
+++ src/test/java/com/tuum/banking/service/RabbitMQPublisherTest.java
@@ -0,0 +1,38 @@
+package com.tuum.banking.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tuum.banking.configuration.exception.JsonConversionException;
+import com.tuum.banking.dto.AccountDto;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class RabbitMQPublisherTest {
+	
+	@Mock
+	ObjectMapper objectMapper;
+	
+	@InjectMocks
+	RabbitMQPublisher rabbitMQPublisher;
+	
+	@Test
+	@SneakyThrows
+	void publishAccountCreated() {
+		Mockito.when(objectMapper.writeValueAsString(any())).thenThrow(JsonProcessingException.class);
+		
+		Throwable thrown = catchThrowable(() -> rabbitMQPublisher.publishAccountCreated(new AccountDto()));
+		
+		assertThat(thrown).isInstanceOf(JsonConversionException.class)
+				.hasMessage("Failed to convert object to JSON");
+	}
+}
