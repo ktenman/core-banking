3113526cd2b2f28df543def82e8983eab2022a12
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-03-18 16:06:34 +0200

    Remove unnecessary lombok annotations and refactor constructor in BalanceConverter and AccountConverter classes

--- src/main/java/com/tuum/banking/converter/AccountConverter.java
+++ src/main/java/com/tuum/banking/converter/AccountConverter.java
@@ -3,11 +3,12 @@ package com.tuum.banking.converter;
 import com.tuum.banking.domain.Account;
 import com.tuum.banking.dto.AccountDto;
 import com.tuum.banking.dto.CreateAccountRequest;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccountConverter {
+	
+	private AccountConverter() {
+	}
+	
 	public static Account toEntity(CreateAccountRequest createAccountRequest) {
 		Account account = new Account();
 		account.setCustomerId(createAccountRequest.getCustomerId());
